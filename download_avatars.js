7c6fc822dddd287e539e43692fe7d16e3c706ebd
sindhupriya-madala <madala.sindhu@gmail.com>
2017-04-11 01:35:04 +0000

    prepared url with GITHUB USERNAME And ACCESS TOKEN

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,11 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
+var GITHUB_USER = "sindhupriya-madala";
+var GITHUB_TOKEN = "7438b7051d4f103c14d43181ecfc53a7a48a03eb";
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+ const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+ console.log(requestURL);
 }
 
 getRepoContributors("jQuery", "jQuery", function(err, result) {
