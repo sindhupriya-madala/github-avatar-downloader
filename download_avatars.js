f9fa434785e0bbafe58a39bb421c03cb75db01ee
sindhupriya-madala <madala.sindhu@gmail.com>
2017-04-11 01:21:24 +0000

    added single function getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jQuery", "jQuery", function(err, result) {
+  console.log("error is , ", error );
+  console.log("result is ,", result);
+});
+
